cb4327bb066eaea764a832942803efc150b708b0
卫志龙WeiZhilong <72422088+1807149205@users.noreply.github.com>
2023-10-04 07:41:37 +0000

    commit

--- vue/src/components/MainComponent.vue
+++ vue/src/components/MainComponent.vue
@@ -9,17 +9,37 @@ interface dataType {
 
 const data = ref<dataType[]>([]);
 
-const fetchData = () => {
-    axios.get('/data/data.json')
-        .then(res => {
-            if (res.data as dataType) {
-                data.value = res.data as dataType[];
-            }
-        })
-        .finally(() => {
-            console.log(data);
-        })
+const fetchData = async () => {
+    const respData = await axios.get('/data/data.json');
+    const finalData: dataType[] = respData.data as dataType[];
+    for (let i = 0 ; i < finalData.length ; i++) {
+        const flag = await checkResourceStatus(finalData[i]);
+        if (flag) {
+            data.value.push(finalData[i]);
+        }
+    }
 }
+
+// const checkVideoResource = async (video: dataType) => {
+//     try {
+//         const response = await fetch(`/video/${video.fileName}`);
+//         return response.status === 200; // 200表示资源存在
+//     } catch (error) {
+//         console.error('资源检查失败：', error);
+//         return false; // 出现错误时假定资源不存在
+//     }
+// }
+const checkResourceStatus = async (video: dataType) => {
+    try {
+        const response = await axios.head(`/video/${video.fileName}`);
+        // 这里只关心响应状态码，不需要响应内容
+        console.log(`视频读取成功！${video.fileName}`)
+        return response.status === 200; // 200表示资源存在
+    } catch (error) {
+        console.error('失败', video.fileName);
+        return false; // 出现错误时假定资源不存在
+    }
+};
 
 const initPage = () => {
     fetchData();
@@ -36,15 +56,27 @@ onMounted(() => {
 
 <template>
 
-<div>
-    <div v-for="video in data">
-        {{ video.fileName }}
-        <video controls :src="`/video/${video.fileName}`"></video>
+<div class="MainContainer">
+    <div style="width:80%">
+        <div v-for="video in data" class="videoCard" style="margin-top: 20px">
+            <span style="color: rgb(43 43 43);">{{ video.fileName }}</span>
+            <video controls :src="`/video/${video.fileName}`"></video>
+        </div>
     </div>
+    
 </div>
 
 </template>
 
 <style scoped>
-
+.MainContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+.videoCard{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
 </style>
