5b7e7cb8c0d55c993117bf99704d050a4a82d6a0
卫志龙WeiZhilong <72422088+1807149205@users.noreply.github.com>
2023-10-05 15:28:28 +0000

    添加菜单

--- vue/src/constant/public.ts
+++ vue/src/constant/public.ts
@@ -1,2 +1,2 @@
-// export const PUBLIC_URL: string = "https://1807149205.github.io/GITHUB_pages";
-export const PUBLIC_URL: string = "/public";
+export const PUBLIC_URL: string = "https://1807149205.github.io/GITHUB_pages";
+// export const PUBLIC_URL: string = "/public";
